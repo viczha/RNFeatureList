4342531d9a84ef7173a124a895c4f425faf0ee25
taozhangt <taozhangt@pptv.com>
2016-03-29 17:36:21 +0800

    new

---
+++ README.md
@@ -0,0 +1,5 @@
+# android
+# create release apk
+cd android && ./gradlew assembleRelease
+# install release apk
+cd android && ./gradlew installRelease
---
+++ index.ios.js
@@ -0,0 +1,43 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ */
+
+import React, {
+  AppRegistry,
+  Component,
+  StyleSheet,
+  Text,
+  View
+} from 'react-native';
+
+var Game2048 = require('./Views/Game2048')
+
+class RNFeatureList extends Component {
+  render() {
+    return (
+        <Game2048></Game2048>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+        backgroundColor: '#F5FCFF',
+    },
+    welcome: {
+        fontSize: 20,
+        textAlign: 'center',
+        margin: 10,
+    },
+    instructions: {
+        textAlign: 'center',
+        color: '#333333',
+        marginBottom: 5,
+    },
+});
+
+AppRegistry.registerComponent('RNFeatureList', () => RNFeatureList);
